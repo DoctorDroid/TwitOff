84cb3faebdcf46a68e3a733f33fbb4ebec01b1cd
Timothy Eakin <61610170+DoctorDroid@users.noreply.github.com>
2020-09-30 23:27:29 -0400

    Add files via upload
    with absolute path

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ def create_app():
     '''Create and configure an instance of our flask app'''
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///twitoff.db'
+    #absolute path = 'sqlite:///C:\\Users\\Timothy Eakin\\PycharmProjects\\pythonProject\\TwitOff\\twitoff.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     DB.init_app(app)
 
@@ -20,4 +21,5 @@ def create_app():
         DB.session.commit()
 
         return f'{username} has been added to the DB'
+
     return app
