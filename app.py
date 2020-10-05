5f283dabd3d94590e0e69f32ba9d44a1a82a1c06
Timothy Eakin <61610170+DoctorDroid@users.noreply.github.com>
2020-10-04 22:57:41 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,33 +1,37 @@
 
-from flask import Flask
+from flask import Flask, render_template, request
 from .db_model import DB, User, Tweet
+from .twitter import add_user_tweepy
+
 
 def create_app():
-    '''Create and configure an instance of our flask app'''
+    '''
+    Create and configure an instance of our flask app
+    '''
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///twitoff.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///twitoff.sqlite3'
     #absolute path = 'sqlite:///C:\\Users\\Timothy Eakin\\PycharmProjects\\pythonProject\\TwitOff\\twitoff.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    DB.init_app(app)
+    DB.init_app(app) #for connecting the app to SQLAlchemy
 
     @app.route('/')
     def root():
-        return 'Welcome to Twitoff'
+        return render_template('base.html', title='Home', users=User.query.all())
 
-    @app.route('/<username>/<followers>')
-    def add_user(username, followers):
-        user = User(username=username, followers=followers)
-        DB.session.add(user)
-        DB.session.commit()
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def add_or_update_user(name=None, message=''):
+        name = name or request.values['user_name']
 
-        return f'{username} has been added to the DB'
+        try:
+            if request.method == "POST":
+                add_user_tweepy(name)
+                message = f"User {name} successfully added!"
+            tweets = User.query.filter(User.username == name).one().tweet
+        except Exception as e:
+            print(f'Error adding {name}: {e}')
+            tweets = []
 
-    @app.route('/tweet/<tweet>/<user_id>')
-    def add_tweet(tweet, user_id):
-        tweet = Tweet(tweet=tweet, user_id=user_id)
-        DB.session.add(tweet)
-        DB.session.commit()
-
-        return f'New tweet has been added to the DB'
+        return render_template('user.html', title=name, tweets=tweets, message=message)
 
     return app
--- db_model.py
+++ db_model.py
@@ -4,16 +4,17 @@ DB = SQLAlchemy()
 
 
 class User(DB.Model):
-    id = DB.Column(DB.Integer, primary_key=True)
+    id = DB.Column(DB.BigInteger, primary_key=True)
     username = DB.Column(DB.String(80), unique=True, nullable=False)
     followers = DB.Column(DB.String(120), unique=True, nullable=False)
-
+    newest_tweet_id = DB.Column(DB.BigInteger, nullable=False)
     def __repr__(self):
         return '<User %r>' % self.username
 
 class Tweet(DB.Model):
-    id = DB.Column(DB.Integer, primary_key=True)
-    tweet = DB.Column(DB.String(120), unique=True, nullable=False)
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    tweet = DB.Column(DB.String(300), unique=True, nullable=False)
+    embedding = DB.Column(DB.PickleType, nullable=False)
     user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweet', lazy=True))
 
@@ -21,5 +22,7 @@ class Tweet(DB.Model):
         return '<Tweet %r>' % self.tweet
 
     '''
-    to create the database
+    to create the database in flask shell terminal :
+    # from twitoff.db_model import DB, User, Tweet
+    # DB.create_all()
     '''
